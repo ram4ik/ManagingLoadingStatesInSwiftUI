43535cf9732b62449c4d8e8cb26dbbcaf90cee79
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-12 09:48:08 +0200

    Initial Commit

---
+++ ManagingLoadingStatesInSwiftUI/ManagingLoadingStatesInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManagingLoadingStatesInSwiftUIApp.swift
+//  ManagingLoadingStatesInSwiftUI
+//
+//  Created by test on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ManagingLoadingStatesInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
